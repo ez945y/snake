e1ca7690eb2edf20f26e579765a623d07766796f
一中 陳 <ez945y@gmail.com>
2023-03-14 22:05:12 +0800

    combine map by yizhong

--- app/src/main/java/ntutifm/traffic/snake/screens/MainScreen.kt
+++ app/src/main/java/ntutifm/traffic/snake/screens/MainScreen.kt
@@ -4,11 +4,8 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.gestures.detectDragGestures
 import androidx.compose.foundation.layout.*
+import androidx.compose.material.*
 import ntutifm.traffic.snake.R
-import androidx.compose.material.Button
-import androidx.compose.material.Card
-import androidx.compose.material.Icon
-import androidx.compose.material.Text
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Refresh
 import androidx.compose.material.icons.filled.Search
@@ -22,7 +19,12 @@ import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.IntOffset
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.viewinterop.AndroidView
 import androidx.navigation.NavController
+import com.mapbox.geojson.Point
+import com.mapbox.maps.CameraOptions
+import com.mapbox.maps.MapView
+import com.mapbox.maps.Style
 import kotlin.math.roundToInt
 
 @Composable
@@ -31,12 +33,9 @@ fun MainScreen(
     openDrawer: () -> Unit,
 ) {
     Box(modifier = Modifier.fillMaxSize()) {
-        var offsetY by remember { mutableStateOf(0f) }
-        val hightMax = -900f
-        val currenHeight = remember { mutableStateOf(300.dp) }
+        
         Box {
-            Image(painterResource(id = R.drawable.map), null, modifier = Modifier
-                .fillMaxSize(), alignment = Alignment.TopCenter)
+            MapboxScreen()
             val text = remember { mutableStateOf("CLICK ME") }
             Button(onClick = {
                 text.value = "CLICKED"
@@ -46,48 +45,88 @@ fun MainScreen(
                 Text(text.value)
             }
         }
-        Box(modifier = Modifier
-            .background(Color.Gray)
-            .fillMaxWidth()
-            .height(currenHeight.value)
+        ViewBox(modifier = Modifier
             .align(
-                Alignment.BottomCenter)) {
-            Card(backgroundColor = Color.White,
-                modifier = Modifier.padding(top = 30.dp, start = 30.dp)) {
-                Row {
-                    val string1 = "承德路"
-                    val string2 = "市民大道"
-                    Icon(Icons.Filled.Search,
-                        contentDescription = null,
-                        modifier = Modifier.padding(top = 3.dp, start = 10.dp))
-                    Icon(Icons.Filled.Refresh,
-                        contentDescription = null,
-                        modifier = Modifier.padding(top = 3.dp, start = 10.dp))
-                    Icon(Icons.Filled.Star,
-                        contentDescription = null,
-                        modifier = Modifier.padding(top = 3.dp, start = 10.dp))
-                    Text(text = "$string1 X $string2",
-                        modifier = Modifier.padding(start = 50.dp, end = 10.dp))
+                Alignment.BottomCenter))
+        
+    }
+}
 
-                }
-            }
-        }
-        Box(
-            Modifier
-                .padding(top = 500.dp, start = 145.dp)
-                .offset { IntOffset(0, offsetY.roundToInt()) }
-                .background(Color.White)
-                .height(10.dp)
-                .width(120.dp)
-                .pointerInput(Unit) {
-                    detectDragGestures { change, dragAmount ->
-                        change.consumeAllChanges()
-                        if ((dragAmount.y > 0 || offsetY > hightMax) && (dragAmount.y < 0 || offsetY < 0f)) {
-                            offsetY += dragAmount.y
-                            currenHeight.value -= dragAmount.y.toDp()
-                        }
+@Composable
+fun MapboxScreen() {
+    Box(
+        contentAlignment = Alignment.Center,
+        modifier = Modifier
+            .background(MaterialTheme.colors.background)
+            .fillMaxSize()
+    ) {
+        AndroidView(
+            factory = { context ->
+                MapView(context)
+            },
+            modifier = Modifier.fillMaxSize()
+        ) { mapView ->
+            mapView
+                .getMapboxMap().apply {
+                    loadStyleUri(Style.MAPBOX_STREETS)
+                    addOnStyleLoadedListener {
+                        setCamera(
+                            CameraOptions.Builder()
+                                .center(Point.fromLngLat(46.0, 16.0))
+                                .zoom(2.0)
+                                .build()
+                        )
                     }
                 }
-        )
+        }
     }
+}
+
+@Composable
+fun ViewBox(modifier: Modifier) {
+    var offsetY by remember { mutableStateOf(0f) }
+    val hightMax = -900f
+    val currenHeight = remember { mutableStateOf(300.dp) }
+    Box(modifier = modifier
+        .height(currenHeight.value)
+        .fillMaxWidth()
+        .background(Color.Gray)
+        ) {
+        Card(backgroundColor = Color.White,
+            modifier = Modifier.padding(top = 30.dp, start = 30.dp)) {
+            Row {
+                val string1 = "承德路"
+                val string2 = "市民大道"
+                Icon(Icons.Filled.Search,
+                    contentDescription = null,
+                    modifier = Modifier.padding(top = 3.dp, start = 10.dp))
+                Icon(Icons.Filled.Refresh,
+                    contentDescription = null,
+                    modifier = Modifier.padding(top = 3.dp, start = 10.dp))
+                Icon(Icons.Filled.Star,
+                    contentDescription = null,
+                    modifier = Modifier.padding(top = 3.dp, start = 10.dp))
+                Text(text = "$string1 X $string2",
+                    modifier = Modifier.padding(start = 50.dp, end = 10.dp))
+
+            }
+        }
+    }
+    Box(
+        modifier = Modifier
+            .padding(top = 580.dp, start = 145.dp)
+            .offset { IntOffset(0, offsetY.roundToInt()) }
+            .background(Color.White)
+            .height(20.dp)
+            .width(120.dp)
+            .pointerInput(Unit) {
+                detectDragGestures { change, dragAmount ->
+                    change.consumeAllChanges()
+                    if ((dragAmount.y > 0 || offsetY > hightMax) && (dragAmount.y < 0 || offsetY < 0f)) {
+                        offsetY += dragAmount.y
+                        currenHeight.value -= dragAmount.y.toDp()
+                    }
+                }
+            }
+    )
 }
